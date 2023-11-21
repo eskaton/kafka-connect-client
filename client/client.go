ff8bbab0c1c1ed81b22fd1cc6cf7ae088c88678a
me@eskaton.ch <me@eskaton.ch>
2023-11-21 21:15:54 +0100

    Added Basic Auth

--- client/client.go
+++ client/client.go
@@ -10,7 +10,9 @@ import (
 )
 
 type Client struct {
-	url string
+	url      string
+	user     string
+	password string
 }
 
 type Status struct {
@@ -19,46 +21,46 @@ type Status struct {
 	ServerMessage string
 }
 
-func New(url string) *Client {
-	return &Client{url: url}
+func New(url string, user string, password string) *Client {
+	return &Client{url: url, user: user, password: password}
 }
 
 func (client *Client) Cluster() (*types.Cluster, *Status, error) {
-	return get[types.Cluster](client.url)
+	return get[types.Cluster](client, "")
 }
 
 func (client *Client) Connectors() (*types.Connectors, *Status, error) {
-	return get[types.Connectors](client.url + "/connectors")
+	return get[types.Connectors](client, "/connectors")
 }
 
 func (client *Client) Connector(name string) (*types.Connector, *Status, error) {
-	return get[types.Connector](client.url + "/connectors/" + name)
+	return get[types.Connector](client, "/connectors/"+name)
 }
 
 func (client *Client) ConnectorStatus(name string) (*types.ConnectorStatus, *Status, error) {
-	return get[types.ConnectorStatus](client.url + "/connectors/" + name + "/status")
+	return get[types.ConnectorStatus](client, "/connectors/"+name+"/status")
 }
 
 func (client *Client) ConnectorPause(name string) (*Status, error) {
-	_, status, err := put[any, any](client.url+"/connectors/"+name+"/pause", []byte{})
+	_, status, err := put[any, any](client, "/connectors/"+name+"/pause", []byte{})
 
 	return status, err
 }
 
 func (client *Client) ConnectorResume(name string) (*Status, error) {
-	_, status, err := put[any, any](client.url+"/connectors/"+name+"/resume", []byte{})
+	_, status, err := put[any, any](client, "/connectors/"+name+"/resume", []byte{})
 
 	return status, err
 }
 
 func (client *Client) ConnectorRestart(name string, includeTasks bool, onlyFailed bool) (*types.ConnectorStatus, *Status, error) {
-	url := client.url + "/connectors/" + name + "/restart?includeTasks=" + strconv.FormatBool(includeTasks) + "&onlyFailed=" + strconv.FormatBool(onlyFailed)
+	url := "/connectors/" + name + "/restart?includeTasks=" + strconv.FormatBool(includeTasks) + "&onlyFailed=" + strconv.FormatBool(onlyFailed)
 
-	return post[types.ConnectorStatus](url, []byte{})
+	return post[types.ConnectorStatus](client, url, []byte{})
 }
 
 func (client *Client) ConnectorDelete(name string) (*Status, error) {
-	return remove(client.url + "/connectors/" + name)
+	return remove(client, "/connectors/"+name)
 }
 
 func (client *Client) ConnectorCreate(connector types.Connector) (*types.Connector, *Status, error) {
@@ -68,7 +70,7 @@ func (client *Client) ConnectorCreate(connector types.Connector) (*types.Connect
 		return nil, nil, err
 	}
 
-	return post[types.Connector](client.url+"/connectors/", content)
+	return post[types.Connector](client, "/connectors/", content)
 }
 
 func (client *Client) ConnectorUpdateConfig(name string, config types.ConnectorConfig) (*Status, error) {
@@ -78,26 +80,26 @@ func (client *Client) ConnectorUpdateConfig(name string, config types.ConnectorC
 		return nil, err
 	}
 
-	_, status, err := put[types.ConnectorConfig, types.Connector](client.url+"/connectors/"+name+"/config", content)
+	_, status, err := put[types.ConnectorConfig, types.Connector](client, "/connectors/"+name+"/config", content)
 
 	return status, err
 }
 
 func (client *Client) Tasks(name string) (*types.Tasks, *Status, error) {
-	return get[types.Tasks](client.url + "/connectors/" + name + "/tasks")
+	return get[types.Tasks](client, "/connectors/"+name+"/tasks")
 }
 
 func (client *Client) TaskStatus(name string, task string) (*types.TaskStatus, *Status, error) {
-	return get[types.TaskStatus](client.url + "/connectors/" + name + "/tasks/" + task + "/status")
+	return get[types.TaskStatus](client, "/connectors/"+name+"/tasks/"+task+"/status")
 }
 
 func (client *Client) TaskRestart(name string, task string) (*Status, error) {
-	_, status, err := post[any](client.url+"/connectors/"+name+"/tasks/"+task+"/restart", []byte{})
+	_, status, err := post[any](client, "/connectors/"+name+"/tasks/"+task+"/restart", []byte{})
 
 	return status, err
 }
 
-func post[T any](url string, body []byte) (*T, *Status, error) {
+func post[T any](client *Client, url string, body []byte) (*T, *Status, error) {
 	var emptyResult T
 
 	request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
@@ -106,11 +108,13 @@ func post[T any](url string, body []byte) (*T, *Status, error) {
 		return nil, nil, err
 	}
 
+	addAuth(client, request)
+
 	request.Header.Set("Content-Type", "application/json")
 
-	client := http.Client{}
+	httpClient := http.Client{}
 
-	response, err := client.Do(request)
+	response, err := httpClient.Do(request)
 
 	if err != nil {
 		return nil, nil, err
@@ -139,40 +143,22 @@ func post[T any](url string, body []byte) (*T, *Status, error) {
 	return &responseObject, &status, nil
 }
 
-func handleHttpError[T any](response *http.Response, _ T) (*T, *Status, error) {
-	responseBody, err := io.ReadAll(response.Body)
-
-	if err == nil && len(responseBody) > 0 {
-		var responseStatus types.Status
-
-		err = json.Unmarshal(responseBody, &responseStatus)
-
-		if err == nil {
-			status := Status{response.Status, response.StatusCode, responseStatus.Message}
-
-			return nil, &status, nil
-		}
-	}
-
-	status := Status{response.Status, response.StatusCode, ""}
-
-	return nil, &status, nil
-}
-
-func put[I any, O any](url string, body []byte) (*O, *Status, error) {
+func put[I any, O any](client *Client, url string, body []byte) (*O, *Status, error) {
 	var emptyResult O
 
-	request, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(body))
+	request, err := http.NewRequest(http.MethodPut, client.url+url, bytes.NewBuffer(body))
 
 	if err != nil {
 		return nil, nil, err
 	}
 
+	addAuth(client, request)
+
 	request.Header.Set("Content-Type", "application/json")
 
-	client := http.Client{}
+	httpClient := http.Client{}
 
-	response, err := client.Do(request)
+	response, err := httpClient.Do(request)
 
 	if err != nil {
 		return nil, nil, err
@@ -201,22 +187,34 @@ func put[I any, O any](url string, body []byte) (*O, *Status, error) {
 	return &responseObject, &status, nil
 }
 
-func remove(url string) (*Status, error) {
-	request, err := http.NewRequest(http.MethodDelete, url, bytes.NewBuffer([]byte{}))
+func remove(client *Client, url string) (*Status, error) {
+	request, err := http.NewRequest(http.MethodDelete, client.url+url, bytes.NewBuffer([]byte{}))
 
 	if err != nil {
 		return nil, err
 	}
 
-	client := http.Client{}
+	addAuth(client, request)
 
-	response, err := client.Do(request)
+	httpClient := http.Client{}
+
+	response, err := httpClient.Do(request)
 
 	return &Status{response.Status, response.StatusCode, ""}, err
 }
 
-func get[T any](url string) (*T, *Status, error) {
-	response, err := http.Get(url)
+func get[T any](client *Client, url string) (*T, *Status, error) {
+	request, err := http.NewRequest(http.MethodGet, client.url+url, bytes.NewBuffer([]byte{}))
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	addAuth(client, request)
+
+	httpClient := http.Client{}
+
+	response, err := httpClient.Do(request)
 
 	if err != nil {
 		return nil, nil, err
@@ -243,4 +241,30 @@ func get[T any](url string) (*T, *Status, error) {
 	}
 
 	return &responseObject, &status, nil
+}
+
+func addAuth(client *Client, request *http.Request) {
+	if len(client.user) > 0 && len(client.password) > 0 {
+		request.SetBasicAuth(client.user, client.password)
+	}
+}
+
+func handleHttpError[T any](response *http.Response, _ T) (*T, *Status, error) {
+	responseBody, err := io.ReadAll(response.Body)
+
+	if err == nil && len(responseBody) > 0 {
+		var responseStatus types.Status
+
+		err = json.Unmarshal(responseBody, &responseStatus)
+
+		if err == nil {
+			status := Status{response.Status, response.StatusCode, responseStatus.Message}
+
+			return nil, &status, nil
+		}
+	}
+
+	status := Status{response.Status, response.StatusCode, ""}
+
+	return nil, &status, nil
 }
